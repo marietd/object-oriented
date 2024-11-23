febb9b80cc9dd85f164fc8c4662ecab0438cb2ec
Marie Tricaud <marie.tricaud@gmail.com>
2024-11-23 15:41:36 +0000

    handle button press for mobile

--- src/CircleButton.tsx
+++ src/CircleButton.tsx
@@ -8,27 +8,39 @@ const CircleButton: React.FC<CircleButtonProps> = ({ onClick }) => {
   const [isPressed, setIsPressed] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
 
-  const handleMouseUp = () => {
+  const handleInteractionStart = () => {
+    setIsPressed(true);
+  };
+
+  const handleInteractionEnd = () => {
     setIsPressed(false);
+    setIsHovering(false);
     onClick?.();
+  };
+
+  const handleInteractionCancel = () => {
+    setIsPressed(false);
   };
 
   return (
     <div className="relative inline-block">
-      <div 
+      <div
         className="w-16 h-16 rounded-full cursor-pointer bg-red-500 overflow-hidden border-3 border-black"
         onMouseEnter={() => setIsHovering(true)}
-        onMouseDown={() => setIsPressed(true)}
-        onMouseUp={handleMouseUp}
         onMouseLeave={() => {
           setIsHovering(false);
           setIsPressed(false);
         }}
+        onMouseDown={handleInteractionStart}
+        onMouseUp={handleInteractionEnd}
+        onTouchStart={handleInteractionStart} // Handle touch start
+        onTouchEnd={handleInteractionEnd} // Handle touch end
+        onTouchCancel={handleInteractionCancel} // Handle touch cancellation
       >
-        <div 
+        <div
           className={`
             w-full h-full rounded-full bg-blue-500
-            transition-transform duration-30 ease-out
+            transition-transform duration-300 ease-out
             ${isPressed ? 'translate-x-2 translate-y-2' : isHovering ? 'translate-x-0.5 translate-y-0.5' : ''}
           `}
         />
