dcf739ccedce1917e27fc517a5c5bd1d9008183d
Marie Tricaud <marie.tricaud@gmail.com>
2024-11-23 16:18:33 +0000

    rename app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import CircleButton from './CircleButton';
 import CircleKnob from './CircleKnob';
